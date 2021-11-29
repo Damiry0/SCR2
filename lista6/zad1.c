e92e483fc33b6f56f7f4524de5f7831e2df467d5
Damian Rys <damian.rys0@gmail.com>
2021-11-29 12:30:41 +0100

    completed buffor sending trough pipe, working task1

--- lista6/zad1.c
+++ lista6/zad1.c
@@ -1,49 +1,48 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <stdlib.h>
 #include <string.h>
-
-int main(int argc,char *argv[])
+#include <fcntl.h>
+#include <stdlib.h>
+#define MaxSize 16
+int main(int argc, char *argv[])
 {
-        FILE    *file;
-        int     fd[2], nbytes; // fd - przechowuje file describtory fd[0]=odczyt/fd[1]=zapis
-        pid_t   childpid; // przechowuje id procesu potomnego
-        char    *string=NULL;
-        char    readbuffer[80]; //bufor dla potoku
+    int fd[2],file, count; // fd - przechowuje file describtory 
+    //fd[0]=odczyt/fd[1]=zapis
+    char readbuffer[MaxSize]; //bufor dla potoku
 
-        pipe(fd); // tworzenie potoku
-        
-        if((childpid = fork()) == -1) // tworzenie procesu potomnego 
+    pipe(fd); // tworzenie potoku
+    pid_t childpid;           // przechowuje id procesu potomnego
+    if((childpid = fork()) == -1) // tworzenie procesu potomnego 
         {
                 perror("Nie udalo sie utorzyc procesu potomnego.zamykam");
                 exit(0);
         }
-
-        file=fopen(argv[1],"r");
-        size_t len=0;
-        while(getline(&string,&len,file)!=-1)
+    if (childpid == 0) //rodzic
+    {
+        close(fd[1]);
+        while ((count = read(fd[0], readbuffer, MaxSize)))
         {
-            printf("%s",string);
+            write(1, " # ", 2 * sizeof(char));
+            write(1, readbuffer, count);
+            write(1, " # ", 2 * sizeof(char));
         }
-        if(childpid == 0)
+        close(fd[0]);
+    }
+    else if(childpid>0) //dziecko
+    {
+        close(fd[0]);
+        file = open(argv[1], O_RDONLY);
+        /* Odczyt danych z potoku */
+        while ((count = read(file, readbuffer, MaxSize)) > 0)
         {
-                /* Child process closes up input side of pipe */
-                close(fd[0]);
-
-                /* Send "string" through the output side of pipe */
-                write(fd[1], string, (strlen(string)+1));
-                exit(0);
+            if(write(fd[1], readbuffer, count)<0);
         }
-        else
-        {
-                /* Parent process closes up output side of pipe */
-                close(fd[1]);
+        close(fd[1]);
+    }
+    else
+    {
+        printf("Something went wrong.Closing.");
+        exit(0);
+     }
 
-                /* Read in a string from the pipe */
-                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
-                printf("#%s#", readbuffer);
-        }
-        
-        return(0);
 }
